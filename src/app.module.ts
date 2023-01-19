04a69d211e7e531c4f205a388444d513762f7715
willcary <w.a.c.ary@gmail.com>
2023-01-19 16:36:12 -0500

    remove Users reference errors in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [UsersModule, TeamsModule],
+  imports: [TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
