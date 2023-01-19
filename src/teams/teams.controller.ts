efe012461fe702017cecc830935fed0f404bf4dc
willcary <w.a.c.ary@gmail.com>
2023-01-19 16:49:33 -0500

    Add pipe to teams.controller.ts

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,8 +1,55 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { Team } from './entities/team.entity';
-import { CreateTeam } from './dto/create-team.dto';
+// import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+// import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+// import { Team } from './entities/team.entity';
+// import { CreateTeam } from './dto/create-team.dto';
+// import { TeamsService } from './teams.service';
+
+// @ApiTags('teams')
+// @Controller('api')
+// export class TeamsController {
+//   constructor(private teamsService: TeamsService) {}
+
+//   @ApiOkResponse({
+//     type: Team,
+//     isArray: true,
+//     description: 'get all teams',
+//   })
+//   @Get('teams')
+//   getTeams(): any {
+//     return this.teamsService.findAll();
+//   }
+
+//   @ApiOkResponse({ type: Team, description: 'get specific team' })
+//   @Get('team/:id')
+//   getTeamById(@Param('id') id: string): any {
+//     return this.teamsService.findById(Number(id));
+//   }
+
+//   @ApiCreatedResponse({ type: Team, description: 'post a new team' })
+//   @Post('teams')
+//   createTeam(@Body() body: CreateTeam): any {
+//     return this.teamsService.createTeam(body);
+//   }
+// }
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateTeam, Team } from './schemas/schemas';
 import { TeamsService } from './teams.service';
+// import { Team } from './types/types';
 
 @ApiTags('teams')
 @Controller('api')
@@ -20,12 +67,20 @@ export class TeamsController {
   }
 
   @ApiOkResponse({ type: Team, description: 'get specific team' })
+  @ApiNotFoundResponse()
   @Get('team/:id')
-  getTeamById(@Param('id') id: string): any {
-    return this.teamsService.findById(Number(id));
+  getTeamById(@Param('id', ParseIntPipe) id: number): Team {
+    const team = this.teamsService.findById(Number(id));
+
+    if (!team) {
+      throw new UnprocessableEntityException();
+    }
+
+    return team;
   }
 
   @ApiCreatedResponse({ type: Team, description: 'post a new team' })
+  @ApiBadRequestResponse()
   @Post('teams')
   createTeam(@Body() body: CreateTeam): any {
     return this.teamsService.createTeam(body);
